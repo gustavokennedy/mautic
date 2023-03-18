b27ed5dabc5604c972485147eb9116d30b311b0c
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2023-03-17 21:53:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
 
+#!/bin/bash
+# Gustavo Kennedy Renkel
+# Overall.Cloud
+
+# Variáveis
+pass='MauticDBPa$$123'
+db_name='Mautic'
+db_user='UsuarioMautic'
+web_root='/var/www/mautic'
+domain='example.com'
+email='gustavo@overall.cloud'
+timezone='America/Sao_Paulo	'
