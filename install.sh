350befee612ed1c4113bae1864f485adfd6d47c7
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2023-03-20 07:25:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ domain='example.com'
 email='gustavo@overall.cloud'
 timezone='America/Sao_Paulo	'
 
+# Verifica usuário
 if [ "$(whoami)" != 'root' ]; then
 	echo $"Você não tem permissão para rodar $0 como usuário comum (non-root). Por favor use sudo"
 		exit 1;
